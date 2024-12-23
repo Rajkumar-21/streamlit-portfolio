e89f592f021aff5c7f909d473494ccc40da07cd1
Rajkumar_R <61075470+Rajkumar-21@users.noreply.github.com>
2024-12-23 04:12:26 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ from PIL import Image
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "Rajkumar_resume.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
+resume_file = current_dir / "assets" / "Rajkumar-Resume.pdf"
+profile_pic = current_dir / "assets" / "picofme.png"
 
 
 # --- GENERAL SETTINGS ---
